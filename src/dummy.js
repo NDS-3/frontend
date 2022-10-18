eecf88d965b928eb667771cf381b3e4dcfe354d5
hyejin <hy2jinl22@gmail.com>
2022-10-18 17:58:54 +0900

    chore: dummy js 제작

---
+++ src/dummy.js
@@ -0,0 +1,20 @@
+export const dummyPumpkinList = [
+  { pumpkinId: 0, imageUrl: "" },
+  { pumpkinId: 1, imageUrl: "" },
+  { pumpkinId: 2, imageUrl: "" },
+  { pumpkinId: 3, imageUrl: "" },
+  { pumpkinId: 4, imageUrl: "" },
+  { pumpkinId: 5, imageUrl: "" },
+  { pumpkinId: 6, imageUrl: "" },
+  { pumpkinId: 7, imageUrl: "" },
+  { pumpkinId: 8, imageUrl: "" },
+  { pumpkinId: 9, imageUrl: "" },
+];
+
+export const dummyPumpkin = {
+  id: 0,
+  imageUrl:
+    "https://images.chosun.com/resizer/aRkMdmnf2bEYLwNOTLe2ZvrHDnQ=/540x405/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/YIARR5OJCFAG5AGRYZYWOGTQKE.jpg",
+  content: "안녕하세요",
+  password: "",
+};
