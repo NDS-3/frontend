6445c3200b11c45b443112f1a66c1676618957d0
hyejin <hy2jinl22@gmail.com>
2022-10-28 09:38:45 +0900

    chore: 이미지 파일 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        background01: "url('/public/img/background02.png')",
-      },
       colors: {
         'main-color': '#473466',
       },
