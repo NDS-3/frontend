72854eb29d5453fda15b0b59a6fcd77ec7b04b49
hyejin <hy2jinl22@gmail.com>
2022-10-28 11:39:34 +0900

    chore: baseUrl http 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const baseURL = "http://localhost:8000";
-const baseURL = "Project-ALB-988978620.ap-northeast-2.elb.amazonaws.com";
+const baseURL =
+  "http://project-alb-988978620.ap-northeast-2.elb.amazonaws.com/";
 
 export const apiClient = axios.create({
   baseURL,
